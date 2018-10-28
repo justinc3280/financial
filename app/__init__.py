44e8d5e5323273c8a33bf914589e43bb9f32faae
Justin Cianci <Justinc328@gmail.com>
2018-10-27 20:08:11 -0400

    create_app function

--- app/__init__.py
+++ app/__init__.py
@@ -9,17 +9,23 @@ migrate = Migrate()
 login = LoginManager()
 login.login_view = 'auth.login'
 
-app = Flask(__name__)
-app.config.from_object(Config)
+def create_app(config_object=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_object)
 
-db.init_app(app)
-migrate.init_app(app, db)
-login.init_app(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login.init_app(app)
 
-from app.auth import auth as auth_bp
-app.register_blueprint(auth_bp, url_prefix='/auth')
+    from app.auth import auth as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
 
-from app.finance import finance as finance_bp
-app.register_blueprint(finance_bp)
+    from app.finance import finance as finance_bp
+    app.register_blueprint(finance_bp)
+
+    from app.jinja import register_jinja_filters
+    register_jinja_filters(app.jinja_env)
+
+    return app
 
 from app import jinja, models
