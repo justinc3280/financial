96af97c34946151469d94947ce7a955aa18e6dff
Justin Cianci <Justinc328@gmail.com>
2019-04-14 21:15:45 -0400

    black format

--- app/jinja.py
+++ app/jinja.py
@@ -1,11 +1,11 @@
-
 def percentage(fraction):
     return '{:.2%}'.format(fraction)
+
 
 def money(amount, show_as_positive=False):
     if amount is None:
         return None
-        
+
     if show_as_positive:
         amount = abs(amount)
 
@@ -14,8 +14,10 @@ def money(amount, show_as_positive=False):
     else:
         return '(${:,.2f})'.format(abs(amount))
 
+
 def date(date):
     return '{:%m/%d/%Y}'.format(date)
+
 
 def int_or_float(number):
     if number.is_integer():
@@ -23,15 +25,18 @@ def int_or_float(number):
     else:
         return number
 
+
 def sort_by_rank(keys):
     return sorted(keys, key=lambda element: element[0].rank)
 
+
 def register_jinja_filters(environment):
-    environment.filters.update({
+    environment.filters.update(
+        {
             'percentage': percentage,
             'money': money,
             'date': date,
             'int_or_float': int_or_float,
-            'sort_by_rank': sort_by_rank
+            'sort_by_rank': sort_by_rank,
         }
     )
